ee876f6bab40fa732a38d9aa1227a8ad0198203d
Eric Cheyne <eric.cheyne@gmail.com>
2024-09-17 00:03:52 -0700

    Updated Main.java and added ATM.java to create user prompts to input data into the system.

--- src/com/simple/atm/Main.java
+++ src/com/simple/atm/Main.java
@@ -1,14 +1,46 @@
 package com.simple.atm;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
 
+        int pinNum;
+        int userOption = 0;
+
         Account account1 = new Account(5000, 1423);
 
-        System.out.println(account1.getBalance());
-        account1.deposit(700);
-        System.out.println(account1.getBalance());
-        account1.withdraw(2000);
-        System.out.println(account1.getBalance());
+        Scanner scanner = new Scanner(System.in);
+
+        ATM atm = new ATM();
+
+        pinNum = scanner.nextInt();
+
+        if (account1.validatePin(pinNum)) {
+            do {
+                atm.menu();
+                userOption = scanner.nextInt();
+                switch (userOption) {
+                    case 1:
+                        atm.withdrawMenu();
+                        double withdrawAmount = scanner.nextDouble();
+                        account1.withdraw(withdrawAmount);
+                        break;
+                    case 2:
+                        atm.depositMenu();
+                        double depositAmount = scanner.nextDouble();
+                        account1.deposit(depositAmount);
+                        break;
+                    case 3:
+                        atm.balanceMenu(account1.getBalance());
+                    case 4:
+                        atm.exitMenu();
+                        break;
+                    default:
+                        atm.invalidMessageMenu();
+                        break;
+                }
+            } while (userOption != 4);
+        }
     }
 }
